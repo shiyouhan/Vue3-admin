c1a79b698709c92932b8cbdc679a840143e3cfc7
shiyouhan <2470752581@qq.com>
2022-05-04 23:00:25 +0800

    feat: 登录后的处理

--- workspace/vue-program/vue3-admin/src/views/login/index.vue
+++ workspace/vue-program/vue3-admin/src/views/login/index.vue
@@ -33,6 +33,7 @@
 import { ref } from 'vue'
 import { validatePassword } from './rules'
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 
 // 数据源
 const loginForm = ref({
@@ -71,6 +72,7 @@ const onChangePwdType = () => {
 const loading = ref(false)
 const loginFormRef = ref(null)
 const store = useStore()
+const router = useRouter()
 const handleLogin = () => {
   loginFormRef.value.validate((valid) => {
     if (!valid) return
@@ -80,7 +82,7 @@ const handleLogin = () => {
       .dispatch('user/login', loginForm.value)
       .then((data) => {
         loading.value = false
-        // router.push('/')
+        router.push('/')
       })
       .catch((err) => {
         console.log(err)
