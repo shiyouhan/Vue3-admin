de2d81e950d299f28c6a89f6f62718254e8795d7
shiyouhan <2470752581@qq.com>
2022-05-05 00:29:21 +0800

    feat: 获取用户基本信息

--- workspace/vue-program/vue3-admin/src/store/modules/user.js
+++ workspace/vue-program/vue3-admin/src/store/modules/user.js
@@ -1,16 +1,21 @@
-import { login } from '@/api/sys'
+// import { login } from '@/api/sys'
+import { login, getUserInfo } from '@/api/sys'
 import md5 from 'md5'
 import { setItem, getItem } from '@/utils/storage'
 import { TOKEN } from '@/constant'
 export default {
   namespaced: true,
   state: () => ({
-    token: getItem(TOKEN) || ''
+    token: getItem(TOKEN) || '',
+    userInfo: {}
   }),
   mutations: {
     setToken(state, token) {
       state.token = token
       setItem(TOKEN, token)
+    },
+    setUserInfo(state, userInfo) {
+      state.userInfo = userInfo
     }
   },
   actions: {
@@ -32,6 +37,12 @@ export default {
             reject(err)
           })
       })
+    },
+    async getUserInfo(context) {
+      const res = await getUserInfo()
+      console.log(res)
+      this.commit('user/setUserInfo', res)
+      return res
     }
   }
 }
