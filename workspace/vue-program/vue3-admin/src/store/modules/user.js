89a10efebce1e489eb810cb3903e8369ee0106fd
shiyouhan <2470752581@qq.com>
2022-05-05 07:56:34 +0800

    feat: 退出登录方案实现

--- workspace/vue-program/vue3-admin/src/store/modules/user.js
+++ workspace/vue-program/vue3-admin/src/store/modules/user.js
@@ -1,8 +1,11 @@
 // import { login } from '@/api/sys'
 import { login, getUserInfo } from '@/api/sys'
 import md5 from 'md5'
-import { setItem, getItem } from '@/utils/storage'
+import { setItem, getItem, removeAllItem } from '@/utils/storage'
 import { TOKEN } from '@/constant'
+// 增加 导入路由
+import router from '@/router'
+
 export default {
   namespaced: true,
   state: () => ({
@@ -43,6 +46,12 @@ export default {
       console.log(res)
       this.commit('user/setUserInfo', res)
       return res
+    },
+    logout() {
+      this.commit('user/setToken', '')
+      this.commit('user/setUserInfo', {})
+      removeAllItem()
+      router.push('/login')
     }
   }
 }
