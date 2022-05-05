4a838a9722f4b2627d220e4411e5db2daf2505a8
shiyouhan <2470752581@qq.com>
2022-05-05 15:03:05 +0800

    feat: screenfull 全屏功能

--- workspace/vue-program/vue3-admin/src/layout/components/Navbar.vue
+++ workspace/vue-program/vue3-admin/src/layout/components/Navbar.vue
@@ -5,6 +5,7 @@
     <div class="right-menu">
       <theme-picker class="right-menu-item hover-effect"></theme-picker>
       <lang-select class="right-menu-item hover-effect" />
+      <screenfull class="right-menu-item hover-effect" />
       <!-- 头像 -->
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
@@ -34,6 +35,7 @@ import Hamburger from '@/components/Hamburger'
 import Breadcrumb from '@/components/Breadcrumb'
 import LangSelect from '@/components/LangSelect'
 import ThemePicker from '@/components/ThemeSelect/index'
+import Screenfull from '@/components/Screenfull'
 import { useStore } from 'vuex'
 import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
