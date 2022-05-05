e06af0bd3a35eaac6a4edc606b6c3175b470fc3d
shiyouhan <2470752581@qq.com>
2022-05-05 14:47:48 +0800

    feat: 处理 element-plus 主题变更

--- workspace/vue-program/vue3-admin/src/views/profile/index.vue
+++ workspace/vue-program/vue3-admin/src/views/profile/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>profile</div>
+  <h1>profile</h1>
+  <el-row>
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
 </template>
