931d387b99bbc159838a9fdbe206192e1eab2e17
shiyouhan <2470752581@qq.com>
2022-05-05 09:02:52 +0800

    fix: 修复菜单中残留问题

--- workspace/vue-program/vue3-admin/src/layout/components/Sidebar/SidebarMenu.vue
+++ workspace/vue-program/vue3-admin/src/layout/components/Sidebar/SidebarMenu.vue
@@ -1,34 +1,21 @@
 <template>
   <!-- 一级 menu 菜单 -->
   <el-menu
-    :uniqueOpened="true"
-    default-active="2"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b"
+    :default-active="activeMenu"
+    :background-color="$store.getters.cssVar.menuBg"
+    :text-color="$store.getters.cssVar.menuText"
+    :active-text-color="$store.getters.cssVar.menuActiveText"
+    :unique-opened="true"
+    router
   >
     <sidebar-item v-for="item in routes" :key="item.path" :route="item"></sidebar-item>
-    <!-- 子集 menu 菜单 -->
-    <!-- <el-submenu index="1">
-      <template #title>
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
-      </template>
-      <el-menu-item index="1-1">选项1</el-menu-item>
-      <el-menu-item index="1-2">选项2</el-menu-item>
-    </el-submenu> -->
-    <!-- 具体菜单项 -->
-    <!-- <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <template #title>导航四</template>
-    </el-menu-item> -->
   </el-menu>
 </template>
 
 <script setup>
 import SidebarItem from './SidebarItem.vue'
 import { computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 import { filterRouters, generateMenus } from '@/utils/route'
 
 const router = useRouter()
@@ -36,5 +23,11 @@ const routes = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   return generateMenus(filterRoutes)
 })
-console.log(JSON.stringify(routes.value))
+// console.log(JSON.stringify(routes.value))
+// 计算高亮 menu 的方法
+const route = useRoute()
+const activeMenu = computed(() => {
+  const { path } = route
+  return path
+})
 </script>
