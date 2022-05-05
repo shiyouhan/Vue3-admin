0538e999a6875a304c11093a0cca3f12984aad10
shiyouhan <2470752581@qq.com>
2022-05-05 16:07:21 +0800

    feat: 修复 HeaderSearch 的关闭和国际化问题

--- workspace/vue-program/vue3-admin/src/utils/i18n.js
+++ workspace/vue-program/vue3-admin/src/utils/i18n.js
@@ -2,3 +2,18 @@ import i18n from '@/i18n'
 export function generateTitle(title) {
   return i18n.global.t('msg.route.' + title)
 }
+import { watch } from 'vue'
+import store from '@/store'
+
+/**
+ *
+ * @param  {...any} cbs 所有的回调
+ */
+export function watchSwitchLang(...cbs) {
+  watch(
+    () => store.getters.language,
+    () => {
+      cbs.forEach(cb => cb(store.getters.language))
+    }
+  )
+}
