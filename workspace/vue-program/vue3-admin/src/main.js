89fd645f504ee45612a9a01ebb5e792bd385f513
shiyouhan <2470752581@qq.com>
2022-05-07 20:52:24 +0800

    fix: 全局属性处理时间展示问题

--- workspace/vue-program/vue3-admin/src/main.js
+++ workspace/vue-program/vue3-admin/src/main.js
@@ -12,8 +12,12 @@ import installIcons from '@/icons'
 import './permission'
 // 导入 i18n
 import i18n from '@/i18n'
+// filter
+import installFilter from '@/filter'
 
 const app = createApp(App)
+
+installFilter(app)
 installElementPlus(app)
 installIcons(app)
 app.use(store).use(router).use(i18n).mount('#app')
