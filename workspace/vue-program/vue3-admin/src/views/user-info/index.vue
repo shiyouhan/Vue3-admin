c7612e3ed632f622dd345bdeefc0a202af9931b1
shiyouhan <2470752581@qq.com>
2022-05-07 22:23:25 +0800

    feat: 获取展示数据

--- workspace/vue-program/vue3-admin/src/views/user-info/index.vue
+++ workspace/vue-program/vue3-admin/src/views/user-info/index.vue
@@ -1,3 +1,25 @@
 <template>
   <div>user-info</div>
 </template>
+
+<script setup>
+import { userDetail } from '@/api/user-manage'
+import { watchSwitchLang } from '@/utils/i18n'
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true
+  }
+})
+
+// 数据相关
+const detailData = ref({})
+const getUserDetail = async () => {
+  detailData.value = await userDetail(props.id)
+}
+getUserDetail()
+// 语言切换
+watchSwitchLang(getUserDetail)
+</script>
